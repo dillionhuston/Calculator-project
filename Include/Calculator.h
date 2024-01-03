551bdde22d21561b2d1c16f2ccc98b16703436d0
Dillion Huston <122535848+dillionhuston@users.noreply.github.com>
2024-01-03 19:05:21 +0000

    Add files via upload

---
+++ Include/Calculator.h
@@ -0,0 +1,73 @@
+#pragma once
+#define CALCULATOR_H_H
+
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+
+
+bool bIsinMenu;
+
+ void getoption(int option);
+
+	class menu
+	{
+	public:
+
+	int option;
+	void show();
+
+
+	};
+
+	
+	class Addition
+	{
+	public:
+
+		int num1, num2;
+		int answer;
+
+		void add();
+
+		Addition *addition;
+
+	};
+
+	class Multipication {
+
+	public:
+		unsigned int num1, num2;
+		int answer;
+
+		void mutiply();
+	};
+
+
+	class Division {
+
+	public:
+		unsigned int num1, num2;
+		int answer;
+
+		void Divide();
+	};
+
+
+
+	class Subtraction {
+
+	public:
+		unsigned int num1, num2;
+		int answer;
+
+		void Subtract();
+	};
+
+	//spawn object of class 
+	menu Menu; // Menu class object
+	Addition Add;
+	Multipication mulitplication;
+	Division division;
+	Subtraction subtraction;
